2a955752fb9170f9eba310ae69e72b2b21660e36
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-10-29 15:50:59 -0300

    last changes

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,6 @@ const App = () => {
     
         <Header/>
         <main className='mt-16'>
-          {/* { mobileNavigationModel &&
-            <ModelMobileNavigation/>
-          } */}
           <Outlet/>
         </main>
         <Footer/>
