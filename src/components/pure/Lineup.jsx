eceadde5fdc9c0f89cd38cc56f126bc7d48e1b2c
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-11-16 00:44:33 -0300

    change LineUp images

--- src/components/pure/Lineup.jsx
+++ src/components/pure/Lineup.jsx
@@ -1,6 +1,31 @@
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
 
+
+const Notebook = require('../../assets/lineup/noteLineup.jpg')
+const Smartphone = require('../../assets/lineup/smartphoneLineup.jpg')
+const Headphones = require('../../assets/lineup/hearphoneLineup.webp')
+const Smartwatch = require('../../assets/lineup/smartwhachLineup.webp')
+
+const lineupProduct = [
+  {
+    name: 'Notebook',
+    img: Notebook
+  },
+  {
+    name: 'Smartphone',
+    img: Smartphone
+  },
+  {
+    name: 'Headphones',
+    img: Headphones
+  },
+  {
+    name: 'Smartwatch',
+    img: Smartwatch
+  }
+]
+
 const LineupComponent = () => {
 
     const nativage = useNavigate()
@@ -8,25 +33,6 @@ const LineupComponent = () => {
     const goProducts = (categorie) => {
       nativage(`/product/${categorie}`)
     }
-
-    const lineupProduct = [
-      {
-        name: 'Notebook',
-        img: 'https://cdn.comparez-malin.fr/img/lenovo/2022/30141/lenovo-ideapad-3-15-blue-1.jpg'
-      },
-      {
-        name: 'Smartphone',
-        img: 'https://www.notebookcheck.org/fileadmin/Notebooks/LG/K30/K30_blk_DZ09.jpg'
-      },
-      {
-        name: 'Headphones',
-        img: 'https://tienda.claro.com.ar/wcsstore/Claro/images/catalog/productos/646x1000/7003747.jpg'
-      },
-      {
-        name: 'Smartwatch',
-        img: 'https://i5.walmartimages.com/asr/d1ec9478-90c6-4583-ab1a-ae66d8bcf8a0.85aefe7b100e614a8880ce99885f5993.jpeg'
-      }
-    ]
 
     return (
       <>
