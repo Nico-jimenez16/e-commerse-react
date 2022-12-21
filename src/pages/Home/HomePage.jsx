9a9fd5151b11c42fc9732c4c3f03f8f4f500941e
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2022-12-21 16:06:57 -0300

    style title change

--- src/pages/Home/HomePage.jsx
+++ src/pages/Home/HomePage.jsx
@@ -1,16 +1,14 @@
 import React from 'react';
+import '../../styles/title.css'
 import { useNavigate } from 'react-router-dom'
 import Lineup from '../../components/pure/Lineup';
-// import RecommendedProductComponent from '../../components/pure/RecommendedProducts';
 import ContactForm from '../../components/pure/forms/ContactForm';
-
-// import {useServiceProducts} from '../../hooks/useServiceProducts';
 
 const bannerHome = require('../../assets/banner-technology.webp')
 
+
 const HomePage = () => {
 
-    // const { products } = useServiceProducts()
     const navigate = useNavigate()
 
     const goProduct = () => {
@@ -24,28 +22,13 @@ const HomePage = () => {
         <div className='relative w-full h-full lg:h-96 flex justify-center items-center'>
           <img className='w-full h-full object-fill' src={ bannerHome } alt="Banner Home page" />
           <div className='absolute w-full h-full flex flex-col justify-center items-start p-8'>
-            <h1 className='text-5xl text-white md:text-7xl font-serif'>VOTF</h1>
-            <button onClick={ goProduct } className='text-xl text-white border border-cyan-600 p-1 md:p-2 mt-1 md:mt-2 hover:bg-cyan-600 hover:text-white'> Explore Products </button>
+            <span className='text-5xl text-white md:text-7xl style-title'>N-VOTF</span>
+            <button onClick={ goProduct } className='text-xl text-white border border-white p-1 md:p-2 mt-1 md:mt-2 hover:bg-cyan-600 hover:text-white'> Explore Products </button>
           </div>
         </div>
         <div className='w-full h-auto flex flex-col justify-top items-center p-4'>
           <Lineup/>
         </div>
-        {/* <div className='p-4'>
-          <h1 className='w-full flex mt-4 text-lg'>Recommended Products ...</h1>
-          <div className='w-full h-56 flex justify-center items-center rounded-xl border-y p-2 shadow-xl'>
-            {
-              products.map((product) => {
-                return (
-                  <RecommendedProductComponent
-                    key={product.id}
-                    product={product}
-                  />
-                )
-            })
-            }
-          </div>
-        </div> */}
         <div className='w-full flex flex-col justify-center items-center my-8'>
           <ContactForm/>
         </div>
