5ecbe3053dd777fbe512a6fc374a3345169b6313
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2023-01-16 18:11:34 -0300

    confirmation component  - fixed

--- src/components/pure/notifications/confirmation.jsx
+++ src/components/pure/notifications/confirmation.jsx
@@ -10,7 +10,7 @@ const ConfirmationComponent = () => {
   return (
     <>
       {showNotification && 
-        <div className='absolute z-50 w-auto right-10 top-16 rounded-xl h-auto'>
+        <div className='fixed z-50 w-auto right-10 top-16 rounded-xl h-auto'>
           <div className={type==='success' ? 'bg-green-300' : 'bg-red-300'} style={{ padding:'16px' }}>
             <h2 className='text-md text-white font-bold'> { message } </h2>
           </div>
