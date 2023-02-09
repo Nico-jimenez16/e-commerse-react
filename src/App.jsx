6cde9f861546a59da3440eadfece5bc2876a6946
Nico-jimenez16 <jimeneznicolas520@gmail.com>
2023-02-09 15:15:58 -0300

    Revert "subir cambios"
    This reverts commit ca76b1002c47e59e6c4b0d5eae55219bb1e4e061.

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Header from './components/pure/Header';
 import Footer from './components/pure/Footer';
 import ConfirmationComponent from './components/pure/notifications/Confirmation';
 
-// import styles globales
+// import styles 
 import './App.css';
 
 // importando los contextos
